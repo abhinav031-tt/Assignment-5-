7579d00553d9c6b88be141d38fe1ac2e0a8470a5
abhinav031-tt <113112279+abhinav031-tt@users.noreply.github.com>
2022-10-13 23:55:49 +0530

    Add files via upload

---
+++ challenge_3.py
@@ -0,0 +1,25 @@
+
+
+
+class student:
+    def info (self,) :
+     name =  "Ram"
+     rollno = "45"
+    pass
+
+    def setName(self,name):
+         self.name = name
+
+    def getName(self,name):
+        return self.name
+
+    def setRollNumber(self):
+         set.rollno = self.rollno
+
+    def getRollNumber(self):
+        return self.rollno 
+
+obj = student ("Ram" , 45)
+
+obj.getName ()
+obj.getRollNumber()
